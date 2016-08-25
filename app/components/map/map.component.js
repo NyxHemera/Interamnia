0c80dde8717732733a590d4fc7929564c3d538bd
Jordan Rust <jordan.rust92@gmail.com>
2016-08-25 18:27:44 -0400

    Fixed links between files

--- app/components/map/map.component.js
+++ app/components/map/map.component.js
@@ -9,7 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var static_1 = require('/app/objects/static');
+var static_1 = require('app/objects/static');
 var MapComponent = (function () {
     function MapComponent() {
         this.mapArr = [];
